6b379e150b31e0a03a7de34e931db19c0b5cd5bb
nhsy-hcp <126588262+nhsy-hcp@users.noreply.github.com>
2025-02-28 16:18:53 +0000

    Refactoring

--- terraform-cloud/baseline-configuration/vault.tf
+++ terraform-cloud/baseline-configuration/vault.tf
@@ -16,41 +16,6 @@ data "okta_group" "mgmt" {
   name     = each.value
 }
 
-# resource "vault_jwt_auth_backend" "tfc" {
-#   description        = "JWT auth backend for Terraform Cloud"
-#   path               = var.vault_auth_path
-#   bound_issuer       = "https://app.terraform.io"
-#   oidc_discovery_url = "https://app.terraform.io"
-#
-#   tune {
-#     default_lease_ttl = var.default_lease_ttl
-#     max_lease_ttl     = var.max_lease_ttl
-#     token_type        = var.token_type
-#   }
-# }
-
-# resource "vault_jwt_auth_backend_role" "tfc_admin" {
-#   backend         = vault_jwt_auth_backend.tfc.path
-#   bound_audiences = ["vault.workload.identity"]
-#   bound_claims = {
-#     sub = format("organization:%s:project:%s:workspace:%s:run_phase:*",
-#       var.tfc_organization,
-#       var.tfc_project,
-#     var.tfc_workspace)
-#   }
-#   bound_claims_type = "glob"
-#   role_type         = "jwt"
-#   role_name         = var.vault_role
-#   token_type        = "service"
-#   token_policies = [
-#     "default",
-#     "tfc-admin",
-#   ]
-#   token_ttl     = 60
-#   token_max_ttl = 60 * 5
-#   user_claim    = "terraform_full_workspace"
-# }
-
 resource "vault_identity_group" "vault_user" {
   name              = "${data.okta_group.mgmt["vault-user"].name}-external"
   type              = "external"
