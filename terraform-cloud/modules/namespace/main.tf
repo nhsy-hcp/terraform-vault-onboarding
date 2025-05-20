0451a085cf56aff237b0482c7f7f214ed4e5f676
nhsy-hcp <126588262+nhsy-hcp@users.noreply.github.com>
2025-05-20 16:33:35 +0100

    Refactor policies

---
+++ terraform-cloud/modules/namespace/main.tf
@@ -0,0 +1,10 @@
+locals {
+  rbac_policies = flatten([
+    for delegation_key, delegation in var.rbac_delegation : [
+      for policy_key, policy_hcl in delegation.policies : {
+        name   = "${delegation_key}_${policy_key}"
+        policy = policy_hcl
+      }
+    ]
+  ])
+}
