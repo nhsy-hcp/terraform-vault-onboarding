eaaaa328ee168c7281624006c273ae569128f938
nhsy-hcp <126588262+nhsy-hcp@users.noreply.github.com>
2025-05-20 17:04:52 +0100

    Added team secrets engines

--- terraform-cloud/namespace-bu01/main.tf
+++ terraform-cloud/namespace-bu01/main.tf
@@ -9,3 +9,15 @@ module "team1_kv_engine" {
   path        = "team1/"
   description = "Team 1 - KV v2 secrets"
 }
+
+module "team2_kv_engine" {
+  source      = "../modules/kv-engine"
+  path        = "team2/"
+  description = "Team 2 - KV v2 secrets"
+}
+
+module "team3_kv_engine" {
+  source      = "../modules/kv-engine"
+  path        = "team3/"
+  description = "Team 3 - KV v2 secrets"
+}
