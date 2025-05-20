446df9d39a324ae778824485a916d4f4d163e544
nhsy-hcp <126588262+nhsy-hcp@users.noreply.github.com>
2025-05-20 16:54:20 +0100

    Refactor policies

--- terraform-cloud/modules/namespace/vault.tf
+++ terraform-cloud/modules/namespace/vault.tf
@@ -97,9 +97,12 @@ resource "vault_policy" "rbac" {
 # }
 
 resource "vault_identity_group" "rbac_internal" {
-  for_each         = { for p in local.rbac_policies : p.name => p }
+  for_each         = var.rbac_delegation
   namespace        = vault_namespace.default.path
-  name             = data.okta_group.rbac[each.value.group].name
-  member_group_ids = [vault_identity_group.rbac_external[each.value.group].id]
-  policies         = [vault_policy.rbac[each.value.name].name]
+  name             = data.okta_group.rbac[each.key].name
+  member_group_ids = [vault_identity_group.rbac_external[each.key].id]
+  policies = [
+    for policy_key in keys(each.value.policies) :
+    vault_policy.rbac["${each.key}-${policy_key}"].name
+  ]
 }
